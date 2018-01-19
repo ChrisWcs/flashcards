7c6fe41e5d6ed59093108f4107be7574094a6908
Christian <ChrisWcs@users.noreply.github.com>
2018-01-19 12:30:30 -0700

    Add styled-components and create three styledcomps

---
+++ src/styledcomps/FlexRowStart.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const FlexRowStart = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    flex-wrap: wrap;
+`;
+
+export default FlexRowStart;
